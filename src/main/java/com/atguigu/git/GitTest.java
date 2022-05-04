d2ade93c9208d8ba67ba30f2d043ff999c6dfca0
daiwenjian <daiwenjian@koz.com>
2022-05-04 21:27:54 +0800

    11111111111

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git3master!");
         System.out.println("hello git3fix!");
+        System.out.println("hello git3!");
     }
 }
